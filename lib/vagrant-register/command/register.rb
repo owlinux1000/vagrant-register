144f668abaf833982cd4d2cb3978ec39f445f0d0
encry1024 <register.chihiro@gmail.com>
2018-07-22 22:42:45 +0900

    pwd test

--- lib/vagrant-register/command/register.rb
+++ lib/vagrant-register/command/register.rb
@@ -6,27 +6,25 @@ module VagrantRegister
   module Command
     
     class CommandRegister < Vagrant.plugin("2", :command)
+
+      def self.synopsis
+        "register with vagrant-anywhere"
+      end
       
       def execute
+        
         opts = OptionParser.new do |o|
-          o.banner = "Usage: vagrant register [machine-name]"
+          o.banner = "Usage: vagrant register"
         end
         
         # Parse the options
         argv = parse_options(opts)
         return if !argv
 
-        results = []
-        with_target_vms(argv) do |machine|
-          result = ""
-          result << machine.name.to_s << "\n"
-          result << JSON.pretty_generate(machine.config.vm)
-          results << result
-        end
-
-        @env.ui.info(results.join("\n"))
+        @env.ui.info(`pwd`)
         # Success, exit status 0
         0
+        
       end
       
     end
